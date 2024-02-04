05a9972a2c4ab15e78c6e6ea2154ce823685581c
manishbhatt98 <manish.cse15@nituk.ac.in>
2024-02-04 16:37:26 +0530

    HashMap Approach to find the maximum Length

---
+++ NeetCode150/SlidingWindow/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,41 @@
+package NeetCode150.SlidingWindow;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+class LongestRepeatingCharacterReplacement {
+    // private int mostFrequentCharacterLength(Map<Character, Integer> map) {
+    //     return Collections.max(map.values());
+    // }
+
+    // Using HashMap
+    // public int characterReplacement(String s, int k) {
+    //     int len = s.length(), front = 0, rear = 0, result = 0;
+    //     Map<Character, Integer> map = new HashMap<>();
+    //     map.put(s.charAt(0), 1);
+    //     while(front<len) {
+    //         int windowLength = front-rear+1;
+    //         if(windowLength - mostFrequentCharacterLength(map) <= k) {
+    //             result = Math.max(result, windowLength);
+    //             front++;
+    //             if(front < len) map.merge(s.charAt(front), 1, Integer::sum);
+    //         } else {
+    //             char c = s.charAt(rear);
+    //             map.put(c, map.get(c)-1);
+    //             rear++;
+    //         }
+    //     }
+    //     return result;
+    // }
+
+    // Using Array
+
+
+    public static void main(String[] args) {
+        LongestRepeatingCharacterReplacement longestRepeatingCharacterReplacement = new LongestRepeatingCharacterReplacement();
+        System.out.println(longestRepeatingCharacterReplacement.characterReplacement("ABBAB", 2));
+    }
+    
+    // Test for ABBAB
+}
