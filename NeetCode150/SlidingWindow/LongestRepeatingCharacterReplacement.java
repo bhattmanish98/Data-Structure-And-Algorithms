cb5da12af3b6ec97eead33d58012295656cf5509
manishbhatt98 <manish.cse15@nituk.ac.in>
2024-02-04 16:37:46 +0530

    HashMap Approach to find the maximum Length

--- NeetCode150/SlidingWindow/LongestRepeatingCharacterReplacement.java
+++ NeetCode150/SlidingWindow/LongestRepeatingCharacterReplacement.java
@@ -5,31 +5,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 class LongestRepeatingCharacterReplacement {
-    // private int mostFrequentCharacterLength(Map<Character, Integer> map) {
-    //     return Collections.max(map.values());
-    // }
+    private int mostFrequentCharacterLength(Map<Character, Integer> map) {
+        return Collections.max(map.values());
+    }
 
     // Using HashMap
-    // public int characterReplacement(String s, int k) {
-    //     int len = s.length(), front = 0, rear = 0, result = 0;
-    //     Map<Character, Integer> map = new HashMap<>();
-    //     map.put(s.charAt(0), 1);
-    //     while(front<len) {
-    //         int windowLength = front-rear+1;
-    //         if(windowLength - mostFrequentCharacterLength(map) <= k) {
-    //             result = Math.max(result, windowLength);
-    //             front++;
-    //             if(front < len) map.merge(s.charAt(front), 1, Integer::sum);
-    //         } else {
-    //             char c = s.charAt(rear);
-    //             map.put(c, map.get(c)-1);
-    //             rear++;
-    //         }
-    //     }
-    //     return result;
-    // }
-
-    // Using Array
+    public int characterReplacement(String s, int k) {
+        int len = s.length(), front = 0, rear = 0, result = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put(s.charAt(0), 1);
+        while(front<len) {
+            int windowLength = front-rear+1;
+            if(windowLength - mostFrequentCharacterLength(map) <= k) {
+                result = Math.max(result, windowLength);
+                front++;
+                if(front < len) map.merge(s.charAt(front), 1, Integer::sum);
+            } else {
+                char c = s.charAt(rear);
+                map.put(c, map.get(c)-1);
+                rear++;
+            }
+        }
+        return result;
+    }
 
 
     public static void main(String[] args) {
