24acfdf0b524e6af83621da5dfa6521282d0c3df
manishbhatt98 <manish.cse15@nituk.ac.in>
2024-02-09 19:19:33 +0530

    formatting

--- NeetCode150/1D-DynamicProgramming/PerfectSquare.java
+++ NeetCode150/1D-DynamicProgramming/PerfectSquare.java
@@ -7,9 +7,7 @@ class PerfectSquare {
         memory[1] = 1;
         for(int i=2;i<=n;i++) {
             int min = Integer.MAX_VALUE;
-            for(int j=1;j*j<=i;j++) {
-                min = Math.min(min, 1 + memory[i-j*j]);
-            }
+            for(int j=1;j*j<=i;j++) min = Math.min(min, 1 + memory[i-j*j]);
             memory[i] = min;
         }
         return memory[n];
