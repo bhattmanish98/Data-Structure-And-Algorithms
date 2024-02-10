71e7613061e5ac1f16c050644cad48ca6fed9b36
manishbhatt98 <manish.cse15@nituk.ac.in>
2024-02-10 21:39:06 +0530

    MergeTwoSortedList

---
+++ NeetCode150/LinkedList/MergeTwoSortedList.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        ListNode currentNode = null;
+        if (list1.val <= list2.val) {
+            currentNode = list1;
+            list1 = list1.next;
+        }
+        else {
+            currentNode = list2;
+            list2 = list2.next;
+        }
+        ListNode head = currentNode;
+        while(list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                currentNode.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+        }
+        if (list1 == null) currentNode.next = list2;
+        else currentNode.next = list1;
+        return head;
+    }
+}
